b9f2764d69c348ba1770f99a1fd66e1692eaa71c
Henrik Thostrup Jensen <htj@ndgf.org>
2010-06-14 11:37:01 +0000

    add postgres script to setup.py and update documentation to include postgres migration

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(name='sgas-luts-client',
-      version='3.1.0-svn',
+      version='3.1.1-svn',
       description='SGAS LUTS Client',
       author='Henrik Thostrup Jensen',
       author_email='htj@ndgf.org',
       url='http://www.sgas.se/',
       packages=['sgas', 'sgas/client'],
-      scripts=['sgas-ur-register', 'sgas-ur-update', 'sgas-db-migrate']
+      scripts=['sgas-ur-register', 'sgas-ur-update', 'sgas-db-migrate', 'sgas-postgres-migrate']
 
 )
 

e40b4ac1e4f46ac7d0b4fd9e6cbf288491b23b10
Henrik Thostrup Jensen <htj@ndgf.org>
2010-08-25 13:41:56 +0000

    set version number in luts3 client trunk to be svn and date

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
+import time
+
 from distutils.core import setup
 
+gmt = time.gmtime()
+day = '%04d%02d%02d' % (gmt.tm_year, gmt.tm_mon, gmt.tm_mday)
+
+version='3-svn-%s' % day
+
 setup(name='sgas-luts-client',
-      version='3.1.2-svn',
+      version=version,
       description='SGAS LUTS Client',
       author='Henrik Thostrup Jensen',
       author_email='htj@ndgf.org',
